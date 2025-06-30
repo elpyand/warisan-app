16ddb237cfc2f675c8447e664c98d30b1330b421
Piki <pikialpian5@gmail.com>
2025-06-30 09:05:18 +0700

    Fix Vercel configuration for Laravel

---
+++ api/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// File: /api/index.php
+
+// Memberitahu Vercel untuk memuat dan menjalankan file index.php
+// yang ada di dalam folder public Laravel.
+require __DIR__ . '/../public/index.php';
